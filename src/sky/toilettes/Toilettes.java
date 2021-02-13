2345bf60ef56ffddf64b2d6edc4c170e4f0e11c3
pjskyman <pjskyman@free.fr>
2021-02-13 16:51:25 +0100

    Déplacement des requêtes SQL vers la librairie housecommon.

--- src/sky/toilettes/Toilettes.java
+++ src/sky/toilettes/Toilettes.java
@@ -1,20 +1,14 @@
 package sky.toilettes;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.function.ToDoubleFunction;
-import sky.housecommon.Database;
 import sky.housecommon.InstantaneousConsumption;
 import sky.housecommon.Logger;
 import sky.housecommon.NotAvailableDatabaseException;
 import sky.housecommon.PricingPeriod;
+import sky.housecommon.Requestor;
 import sky.housecommon.Temperature;
 import sky.program.Duration;
 
@@ -120,7 +114,7 @@ public class Toilettes
 
     private static void refreshPricingPeriod()
     {
-        List<InstantaneousConsumption> instantaneousConsumptions=loadLastInstantaneousConsumption();
+        List<InstantaneousConsumption> instantaneousConsumptions=Requestor.getLastInstantaneousConsumptions(1);
         if(instantaneousConsumptions.isEmpty())
         {
             try
@@ -130,7 +124,7 @@ public class Toilettes
             catch(InterruptedException e)
             {
             }
-            instantaneousConsumptions=loadLastInstantaneousConsumption();
+            instantaneousConsumptions=Requestor.getLastInstantaneousConsumptions(1);
             if(instantaneousConsumptions.isEmpty())
             {
                 Logger.LOGGER.error("Unable to get the last measure");
@@ -144,116 +138,6 @@ public class Toilettes
         }
         else
             Logger.LOGGER.info("Now the pricing period is "+currentPricingPeriod.name()+" instead of "+oldPricingPeriod.name());
-    }
-
-    private static List<InstantaneousConsumption> loadLastInstantaneousConsumption()
-    {
-        try
-        {
-            try(Connection connection=Database.getEcocompteurConnection())
-            {
-                long startTime=System.currentTimeMillis();
-                List<InstantaneousConsumption> instantaneousConsumptions=new ArrayList<>();
-                try(Statement statement=connection.createStatement(ResultSet.TYPE_FORWARD_ONLY,ResultSet.CONCUR_READ_ONLY))
-                {
-                    try(ResultSet resultSet=statement.executeQuery("SELECT * FROM instantaneous_consumption ORDER BY time DESC LIMIT 1;"))
-                    {
-                        while(resultSet.next())
-                        {
-                            long time=resultSet.getLong("time");
-                            PricingPeriod pricingPeriod=PricingPeriod.getPricingPeriodForCode(resultSet.getInt("pricingPeriod"));
-                            double blueDayOffPeakHourTotal=resultSet.getDouble("blueDayOffPeakHourTotal");
-                            double blueDayPeakHourTotal=resultSet.getDouble("blueDayPeakHourTotal");
-                            double whiteDayOffPeakHourTotal=resultSet.getDouble("whiteDayOffPeakHourTotal");
-                            double whiteDayPeakHourTotal=resultSet.getDouble("whiteDayPeakHourTotal");
-                            double redDayOffPeakHourTotal=resultSet.getDouble("redDayOffPeakHourTotal");
-                            double redDayPeakHourTotal=resultSet.getDouble("redDayPeakHourTotal");
-                            String consumer1Name=resultSet.getString("consumer1Name");
-                            int consumer1Consumption=resultSet.getInt("consumer1Consumption");
-                            String consumer2Name=resultSet.getString("consumer2Name");
-                            int consumer2Consumption=resultSet.getInt("consumer2Consumption");
-                            String consumer3Name=resultSet.getString("consumer3Name");
-                            int consumer3Consumption=resultSet.getInt("consumer3Consumption");
-                            String consumer4Name=resultSet.getString("consumer4Name");
-                            int consumer4Consumption=resultSet.getInt("consumer4Consumption");
-                            String consumer5Name=resultSet.getString("consumer5Name");
-                            int consumer5Consumption=resultSet.getInt("consumer5Consumption");
-                            String consumer6Name=resultSet.getString("consumer6Name");
-                            int consumer6Consumption=resultSet.getInt("consumer6Consumption");
-                            String consumer7Name=resultSet.getString("consumer7Name");
-                            int consumer7Consumption=resultSet.getInt("consumer7Consumption");
-                            String consumer8Name=resultSet.getString("consumer8Name");
-                            int consumer8Consumption=resultSet.getInt("consumer8Consumption");
-                            String consumer9Name=resultSet.getString("consumer9Name");
-                            int consumer9Consumption=resultSet.getInt("consumer9Consumption");
-                            String consumer10Name=resultSet.getString("consumer10Name");
-                            int consumer10Consumption=resultSet.getInt("consumer10Consumption");
-                            instantaneousConsumptions.add(new InstantaneousConsumption(time,
-                                    pricingPeriod,
-                                    blueDayOffPeakHourTotal,
-                                    blueDayPeakHourTotal,
-                                    whiteDayOffPeakHourTotal,
-                                    whiteDayPeakHourTotal,
-                                    redDayOffPeakHourTotal,
-                                    redDayPeakHourTotal,
-                                    consumer1Name,
-                                    consumer1Consumption,
-                                    consumer2Name,
-                                    consumer2Consumption,
-                                    consumer3Name,
-                                    consumer3Consumption,
-                                    consumer4Name,
-                                    consumer4Consumption,
-                                    consumer5Name,
-                                    consumer5Consumption,
-                                    consumer6Name,
-                                    consumer6Consumption,
-                                    consumer7Name,
-                                    consumer7Consumption,
-                                    consumer8Name,
-                                    consumer8Consumption,
-                                    consumer9Name,
-                                    consumer9Consumption,
-                                    consumer10Name,
-                                    consumer10Consumption));
-                        }
-                    }
-                }
-                Collections.reverse(instantaneousConsumptions);
-//                Logger.LOGGER.info(instantaneousConsumptions.size()+" rows fetched in "+(System.currentTimeMillis()-startTime)+" ms");
-                return instantaneousConsumptions;
-            }
-        }
-        catch(NotAvailableDatabaseException|SQLException e)
-        {
-            Logger.LOGGER.error("Unable to parse the request response ("+e.toString()+")");
-            return new ArrayList<>(0);
-        }
-    }
-
-    private static boolean insertIntoDatabase(Temperature temperature) throws NotAvailableDatabaseException
-    {
-        boolean success=false;
-        try(Connection connection=Database.getToilettesConnection())
-        {
-            try(Statement statement=connection.createStatement(ResultSet.TYPE_FORWARD_ONLY,ResultSet.CONCUR_READ_ONLY))
-            {
-                int result=statement.executeUpdate("INSERT INTO temperature VALUES ("
-                        + temperature.getTime()+","
-                        + temperature.getTemperature()+","
-                        + temperature.getSetPoint()+","
-                        + temperature.getRatio()+","
-                        + (temperature.isHeaterOn()?1:0)+");");
-                if(result==1)
-                    success=true;
-            }
-        }
-        catch(SQLException ex)
-        {
-            Logger.LOGGER.error(ex.toString());
-            throw new NotAvailableDatabaseException(ex);
-        }
-        return success;
     }
 
     private static class DatabasePopulator extends Thread
@@ -301,7 +185,7 @@ public class Toilettes
         {
             try
             {
-                return insertIntoDatabase(temperature);
+                return Requestor.insertTemperature(temperature);
             }
             catch(NotAvailableDatabaseException e)
             {
